f5c032b8ba5fa0d3a6aab2d14c24db644cd11bf9
TERVER AOSU <terveraosu@gmail.com>
2019-09-02 12:30:49 +0100

    add

--- src/javascript/validationModule.js
+++ src/javascript/validationModule.js
@@ -8,10 +8,10 @@ const ValidationModule = (() => {
             input.addEventListener('keydown', (event) => {
                 if (input.value.length < 2){
                     input.classList.add('error');
-                    console.log('The input: ' + input.value + ' is less than required!')
+                    // console.log('The input: ' + input.value + ' is less than required!')
                 }else{
                     input.classList.remove('error');
-                    console.log('The input: ' + input.value + ' is ok!')
+                    // console.log('The input: ' + input.value + ' is ok!')
                 }
             })
         })
